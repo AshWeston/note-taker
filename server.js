6441ef01129f4ad45a7c4a6e354b1df1868f18c8
Ash Weston <ash-w@live.com.au>
2022-02-27 14:55:29 +1000

    added server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+//dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid");
+
+//set up express app
+const app = express();
+// set up port
+const PORT = process.env.PORT || 3001;
+
+// NEED TO FIX FILE PATHS FOR DEVELOP DB HTMLS//
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//SET ROUTES FOR APIS
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Develop/db/db.json"));
+});
+
+// ADD NEW NOTES
+app.post("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("/Develop/db/db.json"));
+  const newNotes = req.body;
+  newNotes.id = uuid.v4();
+  notes.push(newNotes);
+  fs.writeFileSync("/Develop/db/db.json", JSON.stringify(notes));
+  res.json(notes);
+});
+
+//DELETE NOTES
+app.delete("/api/notes/:id", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("/Develop/db/db.json"));
+  const deleteNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+  fs.writeFileSync("/Develop/db/db.json", JSON.stringify(deleteNote));
+  res.json(deleteNote);
+});
+
+//CALL HOME
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+//CALL NOTES
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+
+//START LISTEN
+app.listen(PORT, function () {
+  console.log(`Server started on port ${PORT}`);
+});
